1ff0be7b7ade9eaab2f83db5397c7e64d2b72954
IbadatArora <Ibadat.arora@thecodeinsight.com>
2021-06-04 11:33:17 +0530

    working implementation example of form control with model objects

--- Angular/src/app/employee-create-manage/home.component.ts
+++ Angular/src/app/employee-create-manage/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 import { jobProfile } from '../jobProfile.component'
 import { ActivatedRoute, Router } from '@angular/router'
 import { EmployeeService } from '../core/service/employee.service';
@@ -21,21 +21,27 @@ export class HomeComponent implements OnInit {
     { jobId: 4, jobName: "HR" }
   ];
 
-  employee: Employee = new Employee ;
+
+  employeeForm : FormGroup = this.formBuilder.group(new Employee(),{formArray : false}) ;
 
   constructor(private activatedRoute: ActivatedRoute, 
     private router: Router, 
     private employeeService: EmployeeService,
-    private _snackBar: MatSnackBar) { }
+    private _snackBar: MatSnackBar,
+    private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
     this.validateSessionStorage();
   }
 
+  get employee() : Employee {
+    return this.employeeForm.getRawValue() as Employee ;
+  }
+
   autoFillUserValues() { 
     this._snackBar.open("values will autofill","ok");
     this.employeeService.getEmployeeById(2).subscribe((response : Employee)=> {
-      this.employee = response ;
+      this.employeeForm.patchValue(response) ;
     });
   }
 
